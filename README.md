691ad15055b76b59dbc89c83eaf93e0adbb23b07
Stefano Colarelli <stefano.colarelli@gmail.com>
2024-03-17 20:46:56 +0100

    fuzzy mode, compatibility with Ard r4

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Wiring hardware:
-VIN Servo -> 5v Arduino; 
+Wiring hardware / Collegamento hardware:
+![Wiring hardware](arduino.png)
+
+Hardware list / Lista dell'hardware
+Servomotore a rotazione continua / Continuous Rotation Servo Motor
+Condensatore ceramico da 100 nF / 100 nF ceramic capacitor
+Condensatore elettrolitico da 1 uF / 1 uF electrolytic capacitor
+Regolatore di tensione LDO a 5 V / 5V LDO voltage regulator
+Adattatore di alimentazione DC / DC Power Adapter [linke this one](https://www.digikey.com/en/products/detail/368/1528-1386-ND/5629434).
+Adattatore di alimentazione a parete da 12 V 1A / 12V Wall Power Adapter 
+Arduino UNO (r3 o r4)
+
 GNU Servo -> GNU Arduino; 
-Control PIN Servo -> Digital PIN #6 Arduino .
+Control PIN Servo -> Digital PIN #9 Arduino .
--- sacca.ino
+++ sacca.ino
@@ -1,78 +1,139 @@
-#include <VarSpeedServo.h> 
- 
-VarSpeedServo myservo;    // create servo object to control a servo 
+#include <Servo.h>
+
+Servo myservo;    // crea oggetto servo
 int respiro = 1000;
-int basso = 0;
-int alto = 140;
+int basso = 15;
+int alto = 120;
 int val;
-int tipo=0;
+int tipo=6;
 int speed=30;
+
+int velocita=13;
+float pos = 20;
+int pausalunga=random(6,7); // la prima pausa lunga per il sospiro spontaneo dello stato 0
+int contapausa=0;
  
 void setup() {
-  Serial.begin(115200);
-  myservo.attach(6);  // attaches the servo on pin 9 to the servo object 
+  Serial.begin(9600);
+  //Serial.println(pausalunga);
+  myservo.attach(9);  // servo attached sul pin 9
 } 
  
 void loop() {
-       if (Serial.available()) { // If data is available to read,
-       val = Serial.read(); // read it and store it in val
-       Serial.println(val);
+       if (Serial.available()) { // se la connessione seriale è dispoibile
+       val = Serial.read(); // leggi il dato seriale e salvalo in val
+       //Serial.println(val);
        switch (val) {
-          case 48:
+          case 0:
+          case 48: {
           tipo=0; //good, tasto 0
-          alto=140;
-          speed=30;
-            break;
-          case 49:
+            break;}
+          case 1:
+          case 49: {
           tipo=1; //fair, tasto 1
-          alto=140;
-           speed=30;
-            break;
-          case 50:
+            break;}
+          case 2:
+          case 50: {
           tipo=2; //moderate, tasto 2
-          alto=130;
-            speed=30;
-            break;
-          case 51:
+            break;}
+          case 3:
+          case 51: {
           tipo=3; //poor, tasto 3
-          alto=110;
-            speed=60;
-            break;
-          case 52:
+            break;}
+          case 4:
+          case 52: {
           tipo=4; //very poor, tasto 4
-          alto=90;
-            speed=60;
-            break;
-          case 53:
+            break;}
+          case 5:
+          case 53: {
           tipo=5; //extr poor, tasto 5
-          alto=80;
-            speed=80;
-            break;
+            break;}
+          case 6:
+          case 54: {
+          tipo=6; //speciale in caso di malfunzionamento API, tasto 6
+            break;}
         }
-       delay(10); // Wait 10 milliseconds for next reading
-     } //uso lo swtich per creare una lieve irregolarità naturale nel respiro, che può cambiare ad ogni ciclo rispetto ad ogni input seriale
+       delay(10); // attendi 10ms per la prox lettura
+     }
        switch (tipo) {
           case 0:
-          respiro=random(1900,2300);
+          alto=120;
+          basso=12;
+          respiro=random(500,800);
+          //una volta ogni 2 minuti, porta doppio ciclo a velocità 24
+          velocita=10;
+          contapausa++;
+          //Serial.println(contapausa);
+          if(contapausa>pausalunga){
+            velocita=16;
+            respiro=random(1000,1300);
+            pausalunga=random(48,96);
+            if(contapausa>pausalunga+3){
+            contapausa=0;
+            }
+          }
             break;
           case 1:
-          respiro=random(1500,1800);
+          alto=110;
+          basso=15;
+          respiro=random(350,650);
+          velocita=9;
             break;
           case 2:
-          respiro=random(1100,1300);
+          alto=100;
+          basso=15;
+          respiro=random(250,550);
+          velocita=9;
             break;
           case 3:
-          respiro=random(600,900);
+          alto=100;
+          basso=25;
+          respiro=random(200,400);
+          velocita=9;
             break;
           case 4:
-          respiro=random(500,700);
+          alto=100;
+          basso=25;
+          respiro=0;
+          respiro=random(100,250);
+          velocita=9;
             break;
           case 5:
-          respiro=random(0,300);
+          alto=90;
+          basso=25;
+          respiro=0;
+          velocita=9;
+            break;
+          case 6:
+          alto=120;
+          basso=95;
+          respiro=random(500,800);
+          //una volta ogni 2 minuti, porta doppio ciclo a velocità 24
+          velocita=20;
+          contapausa++;
             break;
         }
-  myservo.write(alto, speed, true);        // move to 180 degrees, use a speed of 30, wait until move is complete
-  delay(respiro);
-  myservo.write(basso, speed, true);        // move to 0 degrees, use a speed of 30, wait until move is complete
-  delay(respiro);
+    for (pos = basso; pos <= alto; pos += 1) { // va da 0 (minimo) al massimo, fino a 180 potenzialmente. basso è la posizione in basso della sacca, alto è quanto sale
+    //Serial.println(pos);
+    myservo.write(pos);              // metti il servo in posizione "pos"
+    delay(velocita-2);                       // aspetti di "velocita" (in ms) dopo che è arrivato in posizione
+  }
+  delay(respiro/2); // micropausa post inspirazione
+    for (pos = alto; pos >= basso; pos -= 1) { // dal massimo (max 180) fino a 0
+      //Serial.print(pos);
+      myservo.write(pos);
+      if(pos>(alto*0.75)){
+        delay(velocita+1);
+        //Serial.println("-2");
+      }else{
+        if(pos<(alto/4)){
+          delay(velocita+4);
+          //Serial.println("+2");
+        }else{
+          delay(velocita+1);
+          //Serial.println("+0");
+        }
+      }
+    }
+  delay(respiro*2); // pausa raddoppiata, per simulare l'inattività post espirazione
 }
